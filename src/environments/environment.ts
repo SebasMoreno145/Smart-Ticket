241adf6d26ecd4453e375385d5c98113e78936dc
Sebastian Moreno <smoreno055@gmail.com>
2021-10-13 19:40:25 -0500

    Primer commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCr38E8o15mtQ6bAJtyfJQQXRKcMgHrghA",
+  authDomain: "smart-ticket-e7c14.firebaseapp.com",
+  projectId: "smart-ticket-e7c14",
+  storageBucket: "smart-ticket-e7c14.appspot.com",
+  messagingSenderId: "445241107851",
+  appId: "1:445241107851:web:ffdaa8845fdc34fbe8fb8e",
+  measurementId: "G-BPZ14DRKH7"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
